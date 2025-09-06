42466fcc8598bf241a08fee9d955ec11322299ce
cyrsiansk <cyrsiadv@gmail.com>
2025-09-06 13:06:58 +0400

    style: fix indentation

--- migrations/0001_create_subscriptions.up.sql
+++ migrations/0001_create_subscriptions.up.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE IF NOT EXISTS subscriptions (
-                                             id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     service_name text NOT NULL,
     price integer NOT NULL,
     user_id uuid NOT NULL,
