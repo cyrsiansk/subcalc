e7acaf8add56b7c91b4a8b75d93f12f715b32374
cyrsiansk <cyrsiadv@gmail.com>
2025-09-06 12:17:26 +0400

    feature: add migrations

---
+++ migrations/0001_create_subscriptions.up.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS subscriptions (
+                                             id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    service_name text NOT NULL,
+    price integer NOT NULL,
+    user_id uuid NOT NULL,
+    start_date date NOT NULL,
+    end_date date NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT now(),
+    updated_at timestamp with time zone NOT NULL DEFAULT now()
+    );
+
+CREATE INDEX IF NOT EXISTS idx_subscriptions_user_id ON subscriptions(user_id);
